258568993824549b5bced92ee4d2a42c863805e5
Lucas <lucasbsb100@gmail.com>
2022-04-05 21:13:08 -0300

    Add cores

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF8304da);
+  static const background = Color(0xFFFFFFFF);
+  static const secundary = Color(0xFF45e658);
+  static const terciary = Color(0xFFfe8100);
+}
